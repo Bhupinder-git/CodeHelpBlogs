2e858e30c4dec38a7a5eb2aaeff278c6777942ee
Bhupinder <bhsingh3214@gmail.com>
2026-08-19 20:14:12 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import Blogs from "../components/Blogs";
 import Pagination from "../components/Pagination";
 
 const Home = () => {
-    return (
-        <div className="min-h-screen w-full flex flex-col items-center overflow-auto bg-Neutral font-sans relative">
-            <div className="pt-18">
-            <Blogs/>
-            </div>
-            <Pagination/>
-        </div>
-    );
-}
+  return (
+    <div className="min-h-screen w-11/12 max-w-300 min-w-0 flex flex-col items-center overflow-auto bg-Neutral font-sans relative m-3 gap-y-3 pt-18">
+      <Blogs />
+      <Pagination />
+    </div>
+  );
+};
 
 export default Home;
